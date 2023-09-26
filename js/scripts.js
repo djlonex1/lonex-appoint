1ef9f0c2a1befdae806bdb9c949c46c3dc16080e
lonex <jaiyolaolumide4@gmail.com>
2023-09-26 14:35:35 +0100

    :add input data entry and background image

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+// selecting the form after submission
+    $("#appointment").submit(function(event) {
+
+// preventing all default event of the form        
+        event = preventDefault()
+
+// collect user entry
+const inputFullName = $("full-name").val()    
+const inputPurpose = $("purpose").val()
+const inputTime = $("time").val()
+const inputDate = $("date").val()
+
+    })
+   
+
+   
+})
