9104884ac3f5a47e121045fd79cfb0c362b90c88
lonex <jaiyolaolumide4@gmail.com>
2023-09-27 12:15:29 +0100

    : add the return message after submission

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,15 @@ const inputPurpose = $("purpose").val()
 const inputTime = $("time").val()
 const inputDate = $("date").val()
 
+//Append the name of the Applicant to the return message
+$(".full-name").html(inputFullName)
+
+
+//show the story
+$(".displayFormat").fadeIn();
+$("#custom").fadeOut();
+
+
     })
    
 
